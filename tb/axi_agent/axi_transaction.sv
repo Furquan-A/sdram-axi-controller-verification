636fc1b92a9c542e5d817f69024ad7c9ed36c34d
Furquan-A <furquan.741@gmail.com>
2026-09-06 21:57:41 -0700

    axi_constraints

--- tb/axi_agent/axi_transaction.sv
+++ tb/axi_agent/axi_transaction.sv
@@ -31,8 +31,52 @@ class axi_transaction extends uvm_sequence_item;
 	resp_type_e                bresp; // One resp for one complete Transaction
 	resp_type_e                rresp[]; // each beat needs one rresp 
 	
+	// constraints 
+	constraint burst_size_c {
+		w_data.size() == length+1;
+		wstrb.size()  == length+1;
+	}
 	
+	constraint fixed_burst_type_c {
+		if (burst == FIXED)
+			length inside {[0:15]};
+	}
+
+	constraint incr_burst_type_c {
+		if (burst == INCR)
+			length inside {[0:255]};
+	}
+
+	constraint wrap_burst_type_c {
+		if (burst == WRAP)
+			length inside {1, 3, 7, 15};
+	}
 	
+	constraint array_size_c {
+		if (op == WRITE) {
+			w_data.size() == length + 1;
+			wstrb.size()  == length + 1;
+		}
+		else {
+			w_data.size() == 0;
+			wstrb.size()  == 0;
+		}
+	}
+	
+	constraint address_range_c {
+		addr inside {[32'h0000_0000 : 32'h01FF_FFFF]}; // 0 - 33,554,431
+	}
+	
+	constraint address_aligned_c {
+		addr[1:0] == 2'b00;
+	}
+	
+	constraint address_4k_boundary_c {
+		if (burst == INCR)
+			addr[11:0] +
+			((length + 1) * (AXI_DATA_WIDTH/8))
+			<= 4096;
+	}
 	
 	
 endclass
